34805f1dff8fd9f7371128fe7c818fe73fb81c43
aorap-hackathon <a.orap@icloud.com>
2024-06-01 17:19:27 +0200

    add test endpoint

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/test", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
