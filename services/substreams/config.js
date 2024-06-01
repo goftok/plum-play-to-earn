537e34d46dfe47b4b6d1b1646c11e7e601a45398
dandrozaur <andrei@tuney.io>
2024-06-01 22:40:20 +0200

    a fix number 12491924391241926

--- services/substreams/config.js
+++ services/substreams/config.js
@@ -1,5 +1,5 @@
 export const officialSubstreamEndpointsUrls = {
     arbitrum: "https://arb-one.streamingfast.io:443",
-    // base: "https://base-mainnet.streamingfast.io:443",
+    base: "https://base-mainnet.streamingfast.io:443",
     optimism: "https://opt-mainnet.streamingfast.io:443",
 }
